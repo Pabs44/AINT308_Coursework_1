f307060ce668fcc89a296ca9fdaef3cbad081d24
Pablo Pelaez <pablopelaez44@gmail.com>
2022-02-03 16:37:45 +0000

    Finished task 2

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,7 +1,7 @@
 //James Rogers Jan 2022 (c) Plymouth University
-#include<iostream>
+#include <iostream>
 #include <fstream>
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
@@ -14,7 +14,10 @@ int main()
 
     //Open output file for angle data
     ofstream DataFile;
-    DataFile.open ("../Task2/Data.csv");
+    DataFile.open("../Task2/Data.csv");
+
+    //Set boundaries for HSV
+    int HueLower = 60, HueUpper = 90, SatLower = 50, SatUpper = 255, ValLower = 50, ValUpper = 255;
 
     //loop through video frames
     while(true){
@@ -34,17 +37,34 @@ int main()
         //======================================================Your code goes here====================================================
         //this code will run for each frame of the video. your task is to find the location of the swinging green target, and to find
         //its angle to the pivot. These angles will be saved to a .csv file where they can be plotted in Excel.
+        Mat FrameHSV;
+        cvtColor(Frame,FrameHSV,COLOR_BGR2HSV);
+        Mat FrameFiltered;
+        Vec3b LowerBound(HueLower, SatLower, ValLower);
+        Vec3b UpperBound(HueUpper, SatUpper, ValUpper);
+        inRange(FrameHSV, LowerBound, UpperBound, FrameFiltered);
 
+        imshow("Filter", FrameFiltered);
 
+        //Calculate center of target
+        Moments m = moments(FrameFiltered, true);
+        Point p(p.x = m.m10/m.m00, p.y = m.m01/m.m00);
+        Point Target(0,0);
+        if(m.m00 != 0) Target = p;
 
+        //Draw point and circle at pivot and center of target respectively
+        circle(Frame, Target, 20, Scalar(0,255,0), 3);
+        circle(Frame, Pivot, 5, Scalar(0,0,255), 5);
+        //Draw crosshair on target circle and line between target and pivot
+        line(Frame, Point(Target.x-30,Target.y), Point(Target.x+30,Target.y), Scalar(0,255,0), 2);
+        line(Frame, Point(Target.x,Target.y-30), Point(Target.x,Target.y+30), Scalar(0,255,0), 2);
+        line(Frame, Pivot, Target, Scalar(0,0,255), 2);
 
-
-
-
-
-
-
-
+        //
+        //double Angle = atan2(Target.y, Pivot.x);
+        double Angle = atan2(Target.x - Pivot.x, Target.y - Pivot.y);
+        cout << Angle * 180 / M_PI << endl;
+        DataFile << Angle << endl;
 
         //==============================================================================================================================
 
